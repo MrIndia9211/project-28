5d20168da85e74cc0c5c1ceb9252e05a061fe979
Raghavmalik123 <71924202+Raghavmalik123@users.noreply.github.com>
2020-12-15 15:54:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,8 @@ var ground;
 var boy;
 var tree;
 var stone;
-var mango1,mango2,mango3;
+var mango1,mango2,mango3,mango4,mango5;
+var sling;
 function preload()
 {
 	
@@ -26,66 +27,54 @@ function setup() {
 	boy = new Boy();
 	tree =new Tree();
 	stone = new Stone();
-	slingShot = new Slingshot(stone.body,{x:95,y:595}); 
-	mango1 = new Mango();
-	mango2 = new Mango();
-	mango3 = new Mango();
-	mango4 = new Mango();
-	mango5 = new Mango();
+	sling = new SlingShot(stone.body,{x:250,y:595}); 
+	mango1 = new Mango(800,300,18);
+	mango2 = new Mango(800,300,18);
+	mango3 = new Mango(730,390,18);
+	mango4 = new Mango(700,390,18);
+	mango5 = new Mango(830,390,18);
 	Engine.run(engine);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background("white");
-  ground.display();
-  boy.display();
-  tree .display();
-  stone.display();
-  slingShot.display();
-  mango1.display();
-  mango2.display();
-  mango3.display();
-  mango4.display();
-  mango5.display();
+	rectMode(CENTER);
 
   
+	background("white")
+	fill('red');
+	textSize(24);
+	text("PRESS SPACE TO GET A SECOND CHANCE TO PLAY", 200,200);
+	ground.display();
+	tree.display();
+	boy.display();
+	stone.display();
+	mango1.display();
+	mango2.display();
+	mango3.display();
+	mango4.display();
+	mango5.display();
+	
+	sling.display();
   
+	
+
+	drawSprites();
+   
+  }
   
-  
-    detectCollision(stone, mango1);
-  detectCollision(stone, mango2);
-  detectCollision(stone, mango3);
-  detectCollision(stone, mango4);
-  detectCollision(stone, mango5);
+  function mouseDragged(){
 
-
-
-  drawSprites();
- 
+    Matter.Body.setPosition(stone.body, {x: mouseX , y: mouseY});
 }
-
-function mouseDragged(){
-    Matter.Body.setPosition(stone.body,{x:mouseX , y:mouseY});
-}
-function mouseReleased(){
-    slingShot.fly()
-}
-function keyPressed(){
+  function mouseReleased(){
+	  sling.fly();
+  }
+  function keyPressed(){
 	if(keyCode === 32){
-	  Matter.Body.setPosition(stone.body,{x:800, y:480});
+	 
 	  sling.attach(stone.body);
 	}
-}
-function detectCollision(lstone,lmango){
-  stoneBodyPosition = lstone.body.position;
-  mangoBodyPosition = lmango.body.position;
-
-  var distance = dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y);
-  if(distance <= lmango.r + lstone.r){
-    Matter.Body.setStatic(lmango.body, false);
   }
-
-}
+  
