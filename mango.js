eb53a135843f090bad2f970df63f259fc203de6a
Raghavmalik123 <71924202+Raghavmalik123@users.noreply.github.com>
2020-12-15 15:53:31 +0530

    Update mango.js

--- mango.js
+++ mango.js
@@ -1,36 +1,27 @@
-class Mango {
-    
-    constructor(){
-        var options = {
-            isStatic:true,
-            'restitution':0,
-            'friction':1
-            
-        }
-        this.mango1=Bodies.rectangle(800,300,18,90,options)
-        this.image = loadImage("mango.png");
-        this.mango2=Bodies.rectangle(800,380,18,70,options)
-        this.image = loadImage("mango.png");
-        this.mango3=Bodies.rectangle(730,390,18,70,options)
-        this.image = loadImage("mango.png");
-        this.mango4=Bodies.rectangle(850,380,15,18,options)
-        this.image = loadImage("mango.png");
-        this.mango5=Bodies.rectangle(790,440,10,18,options)
-        this.image = loadImage("mango.png");
-        World.add(world,this.mango1)
-        World.add(world,this.mango2)
-        World.add(world,this.mango3)
-        }
-        
-    display(){
-        imageMode(CENTER);
-       
-        
-        image(this.image,this.mango1.position.x,this.mango1.position.y,20,50)
-        image(this.image,this.mango2.position.x,this.mango2.position.y,20,50)
-        image(this.image,this.mango3.position.x,this.mango3.position.y,20,50)
-        image(this.image,this.mango4.position.x,this.mango4.position.y,20,50)
-        image(this.image,this.mango5.position.x,this.mango5.position.y,20,50)
+class Mango{
+	constructor(x,y,r){
+		var options={
+			isStatic:true,
+			restitution :0,
+            friction :1,
+			}
+		this.x = x;
+		this.y = y;
+		this.r = r
+		this.image=loadImage("mango.png")
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
 
-        }
+	display(){
+		var Pos=this.body.position;	
+		push();
+		translate(Pos.x, Pos.y);
+		rotate(this.body.angle)
+		fill("yellow");
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2);
+		pop();
+ }
 }
